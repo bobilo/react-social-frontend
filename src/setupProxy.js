a4259234f25b00cb093924d81ea2047a27cf4520
bonface <bonface@swiftlabs.ai>
2021-09-01 16:27:24 +0300

    update proxy config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,3 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(createProxyMiddleware(["/api", , "/otherApi"], { target: "https://node-social-backend-1990.herokuapp.com" }));
   };
+
+  module.exports = app => {
+    app.use(proxy("/api/*", { target: "https://node-social-backend-1990.herokuapp.com/" }));
+  };
